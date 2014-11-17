58bc0dc4e6cdbc81545c61b2fb0cd30ef0d720c8
munirbe <munir.b@me.com>
2014-11-17 20:45:08 +0100

    commit assignment 2 file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function to calculate the inverse of a matrix
+## inverse is only calculated if a squared matrix is passed
+## and when the inverse hasn't been calculated yet
 
-## Write a short comment describing this function
+## creates the matrix and caches inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## checks if y is an invertible matrix
+    if(dim(x)[1] == dim(x)[2]) {
+    inv <- NULL;
+    set <- function(y) {
+        x <<- y;
+        inv <<- NULL;
+    }
+    get <- function() {
+        x;
+    }
+    setInv <- function(inverse) {
+        inv <<- inverse;
+    }
+    getInv <- function() {
+        inv;
+    }
+    ## 
+    tmp <<- x;
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
+    }
+    else {
+        message("Only square matrices allowed!")
+    }
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of a squared matrix only when
+## a matrix hasn't been changed
+## b inverse of matrix hasn't been calculated yet and
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    sol <- x$getInv();
+    if(!identical(tmp, x$get())) {
+        message("Matrix has been changed!");
+    }
+    else if(!is.null(sol)) {
+        message("Getting cached data...");
+        return(sol);
+    }
+    else {
+        data <- x$get();
+        sol <- solve(data);
+        x$setInv(sol);
+        sol;
+    }
 }
